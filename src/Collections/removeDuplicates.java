808f967a9cf400b5e964800004019b68c85b7260
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-07-10 20:11:31 +0530

    Use LinkedHashSet to remove duplicates while preserving order
    Changed HashSet to LinkedHashSet to maintain insertion order.

--- src/Collections/removeDuplicates.java
+++ src/Collections/removeDuplicates.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
        List<Integer> list = Arrays.asList(1,2,3,4,2,3);
-       Set<Integer> set = new HashSet<Integer>(list);
+       Set<Integer> set = new LinkedHashSet<Integer>(list);
        System.out.println(set);
 }
 }
